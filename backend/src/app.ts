c540e4431429e8313142aca05d40d819d5fe2b70
Mbarndouka O <58604709+mbarndouka@users.noreply.github.com>
2025-01-19 14:33:27 +0100

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,14 @@ const loggingFormat =
   process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  }
+));
 app.use(morgan(loggingFormat));
 
 connectDB()
