8279776b67a5b8b66990038930830ce147b20507
mbarndouka <mmbarndouka@gmail.com>
2025-01-19 21:58:22 +0200

    Add eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": "off",
+      "react/prop-types": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
